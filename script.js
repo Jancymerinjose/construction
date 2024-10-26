26732a0063064d95999d8ef42a211607d18c1a58
jancy merin jose <jancyj935@gmail.com>
2024-10-26 10:46:35 +0530

    my first commit

---
+++ script.js
@@ -0,0 +1,61 @@
+$(document).ready(function()
+		{
+			$(".bg1").mouseover(function()
+			{
+				$(this).css("background-image", "url(img.jpg)");
+			});
+			$(".bg1").mouseout(function()
+			{
+				$(this).css("background-image", "");
+			});
+			$(".bg2").mouseover(function()
+			{
+				$(this).css("background-image", "url(img1.jpg)");
+			});
+			$(".bg2").mouseout(function()
+			{
+				$(this).css("background-image", "");
+			});
+			$(".bg3").mouseover(function()
+			{
+				$(this).css("background-image", "url(img2.jpg");
+			});
+			$(".bg3").mouseout(function()
+			{
+				$(this).css("background-image", "");
+			});
+			$(".bg4").mouseover(function()
+			{
+				$(this).css("background-image", "url(img4.jpg)");
+			});
+			$(".bg4").mouseout(function()
+			{
+				$(this).css("background-image", "");
+			});
+			$(".bg5").mouseover(function()
+			{
+				$(this).css("background-image", "url(img1.jpg)");
+			});
+			$(".bg5").mouseout(function()
+			{
+				$(this).css("background-image", "");
+			});
+			$(".bg6").mouseover(function()
+			{
+				$(this).css("background-image", "url(img.jpg)");
+			});
+			$(".bg6").mouseout(function()
+			{
+				$(this).css("background-image", "");
+			});
+			$(".bg7").mouseover(function()
+			{
+				$(this).css("background-image", "url(img2.jpg)");
+			});
+			$(".bg7").mouseout(function()
+			{
+				$(this).css("background-image", "");
+			});
+
+
+		});
